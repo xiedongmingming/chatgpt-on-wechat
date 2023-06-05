8398bf64bb78d2e2a904d031a9e4f6c3df724ad7
xiedongmingming <986617424@qq.com>
2023-06-05 11:20:48 +0800

    优化

--- config.py
+++ config.py
@@ -163,13 +163,19 @@ config = Config()
 
 
 def load_config():
+    #
     global config
+
     config_path = "./config.json"
+
     if not os.path.exists(config_path):
+        #
         logger.info("配置文件不存在，将使用config-template.json模板")
+
         config_path = "./config-template.json"
 
     config_str = read_file(config_path)
+
     logger.debug("[INIT] config str: {}".format(config_str))
 
     # 将json字符串反序列化为dict类型
@@ -178,12 +184,19 @@ def load_config():
     # override config with environment variables.
     # Some online deployment platforms (e.g. Railway) deploy project from github directly. So you shouldn't put your secrets like api key in a config file, instead use environment variables to override the default config.
     for name, value in os.environ.items():
+
         name = name.lower()
+
         if name in available_setting:
+
             logger.info("[INIT] override config by environ args: {}={}".format(name, value))
+
             try:
+
                 config[name] = eval(value)
+
             except:
+
                 if value == "false":
                     config[name] = False
                 elif value == "true":
@@ -192,7 +205,9 @@ def load_config():
                     config[name] = value
 
     if config.get("debug", False):
+        #
         logger.setLevel(logging.DEBUG)
+
         logger.debug("[INIT] set log level to DEBUG")
 
     logger.info("[INIT] load config: {}".format(config))
